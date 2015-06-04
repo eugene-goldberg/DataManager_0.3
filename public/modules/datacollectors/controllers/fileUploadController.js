acb1b99abe9fa4ac063b028f8ed0fd21ccdad00b
eugene-goldberg <eugene.goldberg117@gmail.com>
2015-06-04 10:01:36 -0500

    fixing initUploader by adding it to the test

--- public/modules/datacollectors/controllers/fileUploadController.js
+++ public/modules/datacollectors/controllers/fileUploadController.js
@@ -24,6 +24,7 @@ angular.module('datacollectors').controller('FileUploadController',
 
                 if(response.environment === 'test'){
                      url = 'http://dctool-lnx.cloudapp.net:3001/api/files';
+                    initUploader(url);
 
                 }
                 if(response.environment === 'development'){

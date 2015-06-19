0ac988043746e50d681d302447387965b4407fe4
eugene-goldberg <eugene.goldberg117@gmail.com>
2015-06-19 11:25:41 -0500

    picked up latest changes

--- config/initMetadata.js
+++ config/initMetadata.js
@@ -46,49 +46,11 @@ db.collections_metadata.update(
     {upsert: true}
 );
 
-
 db.collections_metadata.update(
-    {collectionName: "DC_Facilities"},
-    {
-    $set: {
-        collectionName: "DC_Facilities",
-        dataVersions: [],
-        dataFields: []
-    }
-    },
-    {upsert: true}
-                            );
-
-
-db.collections_metadata.update(
-    {collectionName: "Chart_of_Accounts"},
+    {collectionName: "Headcount_by_Department_Cost_Center_Labor"},
     {
         $set: {
-            collectionName: "Chart_of_Accounts",
-            dataVersions: [],
-            dataFields: []
-        }
-    },
-    {upsert: true}
-);
-
-db.collections_metadata.update(
-    {collectionName: "GL_accounts"},
-    {
-        $set: {
-            collectionName: "GL_accounts",
-            dataVersions: [],
-            dataFields: []
-        }
-    },
-    {upsert: true}
-);
-
-db.collections_metadata.update(
-    {collectionName: "Cost_Center_Master"},
-    {
-        $set: {
-            collectionName: "Cost_Center_Master",
+            collectionName: "Headcount_by_Department_Cost_Center_Labor",
             dataVersions: [],
             dataFields: []
         }
@@ -109,10 +71,63 @@ db.collections_metadata.update(
 );
 
 db.collections_metadata.update(
+    {collectionName: "AccountView_Inventory"},
+    {
+        $set: {
+            collectionName: "AccountView_Inventory",
+            dataVersions: [],
+            dataFields: []
+        }
+    },
+    {upsert: true}
+);
+
+
+db.collections_metadata.update(
+    {collectionName: "DC_Facilities"},
+    {
+    $set: {
+        collectionName: "DC_Facilities",
+        dataVersions: [],
+        dataFields: []
+    }
+    },
+    {upsert: true}
+                            );
+
+
+
+db.collections_metadata.update(
+    {collectionName: "GL_accounts"},
+    {
+        $set: {
+            collectionName: "GL_accounts",
+            dataVersions: [],
+            dataFields: []
+        }
+    },
+    {upsert: true}
+);
+
+
+
+db.collections_metadata.update(
     {collectionName: "NextGen_data"},
     {
         $set: {
             collectionName: "NextGen_data",
+            dataVersions: [],
+            dataFields: []
+        }
+    },
+    {upsert: true}
+);
+
+db.collections_metadata.update(
+    {collectionName: "Profit_and_Loss_data"},
+    {
+        $set: {
+            collectionName: "Profit_and_Loss_data",
             dataVersions: [],
             dataFields: []
         }

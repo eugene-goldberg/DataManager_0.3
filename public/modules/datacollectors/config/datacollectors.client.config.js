c8c85e040ee44178c31d1ae3e5aa0f2928423a3c
eugene-goldberg <eugene.goldberg117@gmail.com>
2015-07-08 17:35:20 -0500

    added specific role requirements to various app routes

--- public/modules/datacollectors/config/datacollectors.client.config.js
+++ public/modules/datacollectors/config/datacollectors.client.config.js
@@ -4,11 +4,11 @@ angular.module('datacollectors').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Data Management', 'datacollectors', 'dropdown', '/datacollectors(/create)?');
-		Menus.addSubMenuItem('topbar', 'datacollectors', 'Upload Documents', 'document-upload');
-		Menus.addSubMenuItem('topbar','datacollectors','Data Explorer','dataexplorer');
+		Menus.addSubMenuItem('topbar', 'datacollectors', 'Upload Documents', 'document-upload',null,true,['admin']);
+		Menus.addSubMenuItem('topbar','datacollectors','Data Explorer','dataexplorer',null,true,['admin']);
 		Menus.addSubMenuItem('topbar', 'datacollectors', 'Salesforce DC Demand', 'sfupdate',null,true,['sfupdate','admin']);
-        Menus.addSubMenuItem('topbar', 'datacollectors', 'Internal DC Demand', 'internal-demand');
-        Menus.addSubMenuItem('topbar', 'datacollectors', 'Update Playcards Data', 'update-playcard');
+        Menus.addSubMenuItem('topbar', 'datacollectors', 'Internal DC Demand', 'internal-demand',null,true,['admin']);
+        Menus.addSubMenuItem('topbar', 'datacollectors', 'Update Playcards Data', 'update-playcard',null,true,['pcupdate','admin']);
         Menus.addSubMenuItem('topbar', 'datacollectors', 'View Playcards', 'view-playcard');
         Menus.addSubMenuItem('topbar', 'datacollectors', 'DC Demand Dashboard', 'dashboard');
 

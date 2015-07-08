1351ef8948ad1d9eba74be3d5ea5ea819c9030a0
eugene-goldberg <eugene.goldberg117@gmail.com>
2015-07-08 17:44:11 -0500

    further locked down View Playcard and Dashboard routes

--- public/modules/datacollectors/config/datacollectors.client.config.js
+++ public/modules/datacollectors/config/datacollectors.client.config.js
@@ -9,8 +9,8 @@ angular.module('datacollectors').run(['Menus',
 		Menus.addSubMenuItem('topbar', 'datacollectors', 'Salesforce DC Demand', 'sfupdate',null,true,['sfupdate','admin']);
         Menus.addSubMenuItem('topbar', 'datacollectors', 'Internal DC Demand', 'internal-demand',null,true,['admin']);
         Menus.addSubMenuItem('topbar', 'datacollectors', 'Update Playcards Data', 'update-playcard',null,true,['pcupdate','admin']);
-        Menus.addSubMenuItem('topbar', 'datacollectors', 'View Playcards', 'view-playcard');
-        Menus.addSubMenuItem('topbar', 'datacollectors', 'DC Demand Dashboard', 'dashboard');
+        Menus.addSubMenuItem('topbar', 'datacollectors', 'View Playcards', 'view-playcard',null,true,['admin','pcviewer']);
+        Menus.addSubMenuItem('topbar', 'datacollectors', 'DC Demand Dashboard', 'dashboard',null,true,['admin','dashviewer']);
 
 		Menus.addMenuItem('topbar','Admin','admin','dropdown','/admin',true,['admin']);
         Menus.addSubMenuItem('topbar', 'admin', 'Admin Tasks', 'admin');

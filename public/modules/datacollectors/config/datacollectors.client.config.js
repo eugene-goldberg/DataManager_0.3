ee80a03a3ae63f5a53ed2a5414e8323bd31d47a7
eugene-goldberg <eugene.goldberg117@gmail.com>
2015-07-15 12:32:39 -0500

    added roles to requesting and reviewing new functionality

--- public/modules/datacollectors/config/datacollectors.client.config.js
+++ public/modules/datacollectors/config/datacollectors.client.config.js
@@ -4,7 +4,7 @@ angular.module('datacollectors').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Data Management', 'datacollectors', 'dropdown', '/datacollectors(/create)?');
-		Menus.addSubMenuItem('topbar', 'datacollectors', 'Upload Documents', 'document-upload',null,true,['admin','data','document-upload']);
+		Menus.addSubMenuItem('topbar', 'datacollectors', 'Upload Documents', 'document-upload',null,true,['admin','document-upload']);
 		Menus.addSubMenuItem('topbar','datacollectors','Data Explorer','dataexplorer',null,true,['admin','data']);
 		Menus.addSubMenuItem('topbar', 'datacollectors', 'Salesforce DC Demand', 'sfupdate',null,true,['sfupdate','admin']);
         Menus.addSubMenuItem('topbar', 'datacollectors', 'Internal DC Demand', 'internal-demand',null,true,['admin']);
@@ -14,9 +14,9 @@ angular.module('datacollectors').run(['Menus',
 		Menus.addSubMenuItem('topbar', 'datacollectors', 'Request Access', 'request-access',null,true,['user']);
 		Menus.addMenuItem('topbar','Admin','admin','dropdown','/admin',true,['admin']);
         Menus.addSubMenuItem('topbar', 'admin', 'Admin Tasks', 'admin');
-		Menus.addMenuItem('topbar','New Functionality','new-functionality','dropdown','/new-functionality',true,['admin']);
-        Menus.addSubMenuItem('topbar','new-functionality','Request New Functionality','request-new-functionality');
+		Menus.addMenuItem('topbar','New Functionality','new-functionality','dropdown','/new-functionality',true,['admin','document-upload','sfupdate','pcupdate','pcviewer','dashviewer']);
+        Menus.addSubMenuItem('topbar','new-functionality','Request New Functionality','request-new-functionality',null,true,['admin','document-upload','sfupdate','pcupdate','pcviewer','dashviewer']);
 
-        Menus.addSubMenuItem('topbar','new-functionality','Review Requests for New Functionality','review-requests-for-new-functionality');
+        Menus.addSubMenuItem('topbar','new-functionality','Review Requests for New Functionality','review-requests-for-new-functionality',null,true,['admin']);
 	}
 ]);
